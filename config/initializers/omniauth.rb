d7c7a1f9c8f2272423e7ce0ad16b1d2983d29061
Adam Nowak <adam.nowak@netguru.pl>
2014-04-19 17:40:50 +0200

    configure omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :google_oauth2, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret,
+    {
+      approval_prompt: 'auto',
+      access_type: 'online',
+      hd: Rails.application.secrets.domain_name
+    }
 end
