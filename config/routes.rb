e1b1826ac1a4859d5c44ddd4aef61f2caf675772
Adam Nowak <lubieniebieski@gmail.com>
2014-10-05 11:59:23 +0200

    rails 4.2 beta

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
-Props::Application.routes.draw do
-   root 'home#index'
-   get 'app' => 'authenticated#main_app'
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'app' => 'authenticated#main_app'
 
-   get '/auth/:provider/callback' => 'sessions#create'
-   get '/signin' => 'sessions#new', as: :signin
-   get '/signout' => 'sessions#destroy', as: :signout
-   get '/auth/failure' => 'sessions#failure'
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', as: :signin
+  get '/signout' => 'sessions#destroy', as: :signout
+  get '/auth/failure' => 'sessions#failure'
 
-   namespace :api do
-     resources :users, only: [:index, :show]
-     resources :props, only: [:index, :create]
-   end
+  namespace :api do
+    resources :users, only: [:index, :show]
+    resources :props, only: [:index, :create]
+  end
 end
