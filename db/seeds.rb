3bd406725d82e17b8a7d8a795e4dbe753f69f2f8
Adam Nowak <lubieniebieski@gmail.com>
2014-10-25 22:07:37 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) can be set in the file .env file.
+require 'ffaker'
+
+puts '== Creating users'
+20.times do
+  User.create!(name: Faker::Name.name,
+               email: Faker::Internet.email)
+end
+
+puts '== Creating props'
+user_ids = User.pluck(:id)
+100.times do
+  user_id = user_ids.sample
+  propser_id = (user_ids - [user_id]).sample
+  body = if [true, false].sample
+           Faker::HipsterIpsum.sentence
+         else
+           Faker::HipsterIpsum.paragraph[0..250]
+         end
+  date = Random.rand(60.days.to_i).seconds.ago
+  Prop.create!(user_id: user_id,
+               propser_id: propser_id,
+               body: body,
+               created_at: date,
+               updated_at: date)
+end
+puts '== DONE!'
