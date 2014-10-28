093151e9e97a341efe1d2a26ddab6a89fbd4bd68
Adam Nowak <lubieniebieski@gmail.com>
2014-10-28 20:43:50 +0100

    go to app if user is present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,13 @@
 class HomeController < ApplicationController
+  before_filter :move_to_app_if_user
+
   def index; end
+
+  private
+
+  def move_to_app_if_user
+    if current_user.present?
+      redirect_to app_path
+    end
+  end
 end
