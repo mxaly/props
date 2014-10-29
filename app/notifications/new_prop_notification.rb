a121c687aa86e599ce42dc9ccb9d3064d3c4247d
Adam Nowak <lubieniebieski@gmail.com>
2014-10-29 08:43:55 +0100

    link to app included in notification message

--- app/notifications/new_prop_notification.rb
+++ app/notifications/new_prop_notification.rb
@@ -1,7 +1,15 @@
 class NewPropNotification < BaseNotification
+  include Rails.application.routes.url_helpers
+
   pattr_initialize :prop_receiver, :prop_giver, :content
 
   def body
-    "#{prop_giver} just gave a prop to *#{prop_receiver}*: _#{content}_"
+    "#{prop_giver} just gave a prop to *#{prop_receiver}*: _#{content}_ - [Check it out!](#{app_domain})"
+  end
+
+  private
+
+  def app_domain
+    root_url(protocol: :https, host: AppConfig.app_domain)
   end
 end
