727ec01cab741e3323f941bf7d24bb787da06f31
Adam Nowak <lubieniebieski@gmail.com>
2014-10-29 08:11:28 +0100

    sanitize output in serializer
    anything else could be trusted on frontend

--- app/serializers/prop_serializer.rb
+++ app/serializers/prop_serializer.rb
@@ -1,6 +1,12 @@
 class PropSerializer < BaseSerializer
+  include ActionView::Helpers::SanitizeHelper
+
   attributes :id, :user, :propser, :body, :created_at
 
   has_one :user
   has_one :propser, serializer: UserSerializer
+
+  def body
+    sanitize object.body, tags: %w(img)
+  end
 end
