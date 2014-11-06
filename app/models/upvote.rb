d9a4fc3e1b1903f39bf1512cc24ebc3e00a1f028
Adam Nowak <lubieniebieski@gmail.com>
2014-11-06 22:00:35 +0100

    validate upvotes

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,4 +1,14 @@
 class Upvote < ActiveRecord::Base
   belongs_to :user
   belongs_to :prop, counter_cache: true
+
+  validates :user_id, uniqueness: { scope: :prop_id }
+  validates :user_id, :prop_id, presence: true
+  validate :can_upvote?, on: :create
+
+  private
+
+  def can_upvote?
+    self.errors.add(:user_id, "You can't upvote your own prop!)") if user.present? && (prop.user == user)
+  end
 end
