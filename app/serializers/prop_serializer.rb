46429719c38036e00cdda946353a0aeb9b5410f2
Adam Nowak <lubieniebieski@gmail.com>
2014-11-06 21:40:37 +0100

    add is_upvote_possible attribute to prop serializer

--- app/serializers/prop_serializer.rb
+++ app/serializers/prop_serializer.rb
@@ -1,12 +1,27 @@
 class PropSerializer < BaseSerializer
   include ActionView::Helpers::SanitizeHelper
 
-  attributes :id, :user, :propser, :body, :created_at
+  attributes :id, :user, :propser, :body, :created_at, :upvotes_count,
+             :is_upvote_possible
 
   has_one :user
   has_one :propser, serializer: UserSerializer
 
   def body
     sanitize object.body, tags: %w(img)
+  end
+
+  def is_upvote_possible
+    !([object.user_id, object.propser_id].include?(scope.id) || user_has_upvoted?)
+  end
+
+  private
+
+  def user_has_upvoted?
+    if object.respond_to?(:user_has_upvoted?)
+      object.user_has_upvoted
+    else
+      object.upvotes.exists?(user_id: scope.id)
+    end
   end
 end
