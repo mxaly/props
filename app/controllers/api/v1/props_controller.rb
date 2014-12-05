fd46f1da93dd5d9ed938030579915fc96b69a7a6
Marcin Dobrowolski <marcin.dobrowolski@netguru.co>
2014-12-05 19:41:19 +0100

    Fix hound issues

--- app/controllers/api/v1/props_controller.rb
+++ app/controllers/api/v1/props_controller.rb
@@ -11,10 +11,10 @@ module Api::V1
 
     def prop_attributes
       user_emails = params.delete(:user_emails)
-      params.merge(:user_ids: User.where(email: user_emails))
+      params.merge(user_ids: User.where(email: user_emails))
 
       propser_email = params.attributes.delete(:propser_email)
-      params.merge(:propser_id: User.find_by(email: propser_email))
+      params.merge(propser_id: User.find_by(email: propser_email))
 
       params.require(:prop).permit(:body, :user_emails, :propser_email)
     end
