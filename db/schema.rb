63be64bfe31bbc725169fb3b29bf3d77d6751e37
Adam Nowak <lubieniebieski@gmail.com>
2014-12-07 17:43:31 +0100

    no admin by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205160736) do
+ActiveRecord::Schema.define(version: 20141207164220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20141205160736) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",      default: true
+    t.boolean  "admin",      default: false
   end
 
 end
