4939038200346bbcd0592140f6ece096dec78d0c
Adam Nowak <lubieniebieski@gmail.com>
2014-12-07 17:17:20 +0100

    limit number of users assigned to prop

--- app/models/prop_receiver.rb
+++ app/models/prop_receiver.rb
@@ -1,4 +1,16 @@
 class PropReceiver < ActiveRecord::Base
+  USER_LIMIT = 9
+  private_constant :USER_LIMIT
+
   belongs_to :prop
   belongs_to :user
+
+  validates :prop_id, :user_id, presence: true
+  validate :user_limit, on: :create
+
+  private
+
+  def user_limit
+    self.errors.add(:user_id, "Sorry, you can't add more users to this prop") if PropReceiver.where(prop_id: prop_id).count > USER_LIMIT
+  end
 end
