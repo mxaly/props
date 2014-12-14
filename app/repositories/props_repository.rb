e143ff6df82af0b45326ad264c7dc5d58f533503
Adam Nowak <lubieniebieski@gmail.com>
2014-12-14 06:49:02 +0100

    messing with validations

--- app/repositories/props_repository.rb
+++ app/repositories/props_repository.rb
@@ -7,14 +7,21 @@ class PropsRepository
 
   def add(attributes)
     user_ids = attributes.delete(:user_ids).split(',')
-    prop = Prop.create(attributes)
-    user_ids.each do |user_id|
-      PropReceiver.create(prop_id: prop.id, user_id: user_id)
-    end
+    prop = Prop.new attributes
+    add_prop_receivers prop, user_ids
+    prop.save
     prop
   end
 
   def search(attributes)
     PropSearch.new attributes
+  end
+
+  private
+
+  def add_prop_receivers(prop, user_ids)
+    user_ids.each do |user_id|
+      prop.prop_receivers.build(user_id: user_id)
+    end
   end
 end
