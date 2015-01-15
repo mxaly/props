7ae9c49497500961f30424304c6ba7fc934be219
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-01-15 18:41:47 +0100

    Update PropsController

--- app/controllers/api/v1/props_controller.rb
+++ app/controllers/api/v1/props_controller.rb
@@ -2,13 +2,18 @@ module Api::V1
   class PropsController < Api::PropsController
     skip_filter :authenticate_user!
     skip_before_filter  :verify_authenticity_token, only: :create
+    before_filter :require_user, only: :create
     check_token_on :index, :total, :create
+    expose(:users_repository) { UsersRepository.new }
 
     private
 
     def current_user
-      token = params[:token]
-      EasyTokens::Token.find_by_value(token).owner
+      users_repository.find_by_email(params[:propser_email]) || GuestUser.new
+    end
+
+    def require_user
+      render json: 'Missing propser email', status: :unprocessable_entity unless current_user.present?
     end
   end
 end
