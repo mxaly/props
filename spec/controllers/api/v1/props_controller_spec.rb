eb9577dbd0da9cd8981608c9b83dd2ec5bbcf99f
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-01-15 18:42:04 +0100

    Update specs

--- spec/controllers/api/v1/props_controller_spec.rb
+++ spec/controllers/api/v1/props_controller_spec.rb
@@ -18,17 +18,35 @@ describe Api::V1::PropsController do
         .and_return(props_repository)
       allow(EasyTokens::Token).to receive(:find_by_value).and_return(token)
       allow(EasyTokens::TokenValidator).to receive(:valid?).with(token.value).and_return(true)
-      get :index, attrs
     end
 
-    it_behaves_like 'a successful JSON request'
+    context "current_user is not present" do
+      before { get :index, attrs }
 
-    it 'passes correct attributes including upvotes status to search method' do
-      expected_attrs = attrs.merge!(show_upvote_status_for_user_id: user.id).except("token")
-      expect(props_repository).to have_received(:search).with(expected_attrs)
+      it_behaves_like 'a successful JSON request'
+
+      it 'passes correct attributes including upvotes status to search method' do
+        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: nil).except("token")
+        expect(props_repository).to have_received(:search).with(expected_attrs)
+      end
+    end
+
+    context "current_user is present" do
+      before do
+        allow(controller).to receive(:current_user).and_return(user)
+        get :index, attrs.merge(propser_email: 'mail@example.com')
+      end
+
+      it_behaves_like 'a successful JSON request'
+
+      it 'passes correct attributes including upvotes status to search method' do
+        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: 5).except("token")
+        expect(props_repository).to have_received(:search).with(expected_attrs)
+      end
     end
 
     it 'returns search results' do
+      get :index, attrs
       results = json_response['props']
       expect(results.class).to be Array
       expect(results.count).to eq 2
@@ -37,7 +55,7 @@ describe Api::V1::PropsController do
   end
 
   describe '#create' do
-    let(:prop_attrs) { { user_ids: '1,2,3', body: 'body', token: token.value }.as_json }
+    let(:prop_attrs) { { user_ids: '1,2,3', body: 'body', token: token.value, propser_email: 'mail@example.com' }.as_json }
     let(:prop) { Prop.new }
     let(:props_repository) { double(:props_repository, add: prop) }
 
@@ -46,19 +64,32 @@ describe Api::V1::PropsController do
         .and_return(props_repository)
       allow(EasyTokens::Token).to receive(:find_by_value).and_return(token)
       allow(EasyTokens::TokenValidator).to receive(:valid?).with(token.value).and_return(true)
-      expect(prop).to receive(:persisted?).and_return(true)
       allow(Slack::Notifier).to receive(:new).and_return(double(ping: true))
-      post :create, prop_attrs
     end
 
     context 'with valid attributes' do
-      let(:prop_attrs) { { user_ids: '1,2,3', body: 'body', token: token.value }.as_json }
+      context 'current_user is present' do
+        before do
+          allow(controller).to receive(:current_user).and_return(user)
+          expect(prop).to receive(:persisted?).and_return(true)
+          post :create, prop_attrs
+        end
 
-      it_behaves_like 'a successful JSON request'
+        it_behaves_like 'a successful JSON request'
 
-      it 'passes correct attributes to repository' do
-        expected_attrs = prop_attrs.merge('propser_id' => user.id).except("token")
-        expect(props_repository).to have_received(:add).with(expected_attrs)
+        it 'passes correct attributes to repository' do
+          expected_attrs = prop_attrs.merge('propser_id' => user.id).except("token").except("propser_email")
+          expect(props_repository).to have_received(:add).with(expected_attrs)
+        end
+      end
+
+      context "current_user is not present" do
+        it 'raises and error' do
+          allow(controller).to receive(:current_user).and_return(nil)
+          post :create, prop_attrs
+          expect(response.body).to eq 'Missing propser email'
+          expect(response.status).to eq 422
+        end
       end
     end
   end
