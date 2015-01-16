b06380c24bf0eb28e96f19ec15bb151ffb11ee11
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-01-16 10:41:04 +0100

    Refactor props controller spec

--- spec/controllers/api/v1/props_controller_spec.rb
+++ spec/controllers/api/v1/props_controller_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe Api::V1::PropsController do
   let(:user) { double(:user, id: 5) }
-  let(:token) { double(EasyTokens::Token, owner: user, value: 'random_string') }
 
   describe '#index' do
     let(:attrs) do
-      { user_id: '1', propser_id: '2', token: token.value }.as_json
+      { user_id: '1', propser_id: '2' }.as_json
     end
     let(:props) { [Prop.new(id: 3), Prop.new(id: 4)] }
     let(:paginated_props) { Kaminari.paginate_array(props) }
@@ -16,22 +15,21 @@ describe Api::V1::PropsController do
     before do
       allow(controller).to receive(:props_repository)
         .and_return(props_repository)
-      allow(EasyTokens::Token).to receive(:find_by_value).and_return(token)
-      allow(EasyTokens::TokenValidator).to receive(:valid?).with(token.value).and_return(true)
+      allow(controller).to receive(:validate_token) { true }
     end
 
-    context "current_user is not present" do
+    context 'current_user is not present' do
       before { get :index, attrs }
 
       it_behaves_like 'a successful JSON request'
 
       it 'passes correct attributes including upvotes status to search method' do
-        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: nil).except("token")
+        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: nil)
         expect(props_repository).to have_received(:search).with(expected_attrs)
       end
     end
 
-    context "current_user is present" do
+    context 'current_user is present' do
       before do
         allow(controller).to receive(:current_user).and_return(user)
         get :index, attrs.merge(propser_email: 'mail@example.com')
@@ -40,7 +38,7 @@ describe Api::V1::PropsController do
       it_behaves_like 'a successful JSON request'
 
       it 'passes correct attributes including upvotes status to search method' do
-        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: 5).except("token")
+        expected_attrs = attrs.merge!(show_upvote_status_for_user_id: 5)
         expect(props_repository).to have_received(:search).with(expected_attrs)
       end
     end
@@ -55,15 +53,14 @@ describe Api::V1::PropsController do
   end
 
   describe '#create' do
-    let(:prop_attrs) { { user_ids: '1,2,3', body: 'body', token: token.value, propser_email: 'mail@example.com' }.as_json }
+    let(:prop_attrs) { { user_ids: '1,2,3', body: 'body', propser_email: 'mail@example.com' }.as_json }
     let(:prop) { Prop.new }
     let(:props_repository) { double(:props_repository, add: prop) }
 
     before do
       allow(controller).to receive(:props_repository)
         .and_return(props_repository)
-      allow(EasyTokens::Token).to receive(:find_by_value).and_return(token)
-      allow(EasyTokens::TokenValidator).to receive(:valid?).with(token.value).and_return(true)
+      allow(controller).to receive(:validate_token) { true }
       allow(Slack::Notifier).to receive(:new).and_return(double(ping: true))
     end
 
@@ -78,12 +75,12 @@ describe Api::V1::PropsController do
         it_behaves_like 'a successful JSON request'
 
         it 'passes correct attributes to repository' do
-          expected_attrs = prop_attrs.merge('propser_id' => user.id).except("token").except("propser_email")
+          expected_attrs = prop_attrs.merge('propser_id' => user.id).except('propser_email')
           expect(props_repository).to have_received(:add).with(expected_attrs)
         end
       end
 
-      context "current_user is not present" do
+      context 'current_user is not present' do
         it 'raises and error' do
           allow(controller).to receive(:current_user).and_return(nil)
           post :create, prop_attrs
