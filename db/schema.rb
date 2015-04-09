a312307bbfd2ba78819dfbc9fedb163ccdf69252
Adam Nowak <adam.nowak@netguru.co>
2015-04-09 18:17:51 +0200

    add archived_at column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409161244) do
+ActiveRecord::Schema.define(version: 20150409161657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,8 @@ ActiveRecord::Schema.define(version: 20150409161244) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",      default: false
+    t.boolean  "admin",       default: false
+    t.datetime "archived_at"
   end
 
 end
