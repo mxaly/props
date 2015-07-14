d17e0d784e10c311f98ce92b56b13bf0948bf951
Rafał Gawlik <rafal.maly@gmail.com>
2015-07-14 23:42:47 +0200

    remove not needed

--- app/assets/javascripts/components/input-wrapper.js.jsx
+++ app/assets/javascripts/components/input-wrapper.js.jsx
@@ -3,11 +3,9 @@ var InputWrapperComponent = React.createClass({
     var cx = React.addons.classSet;
     var classes = cx({
       'form-group': true,
-      'has-error': !!this.props.errors,
-      'has-success': !this.props.errors
+      'has-error': !!this.props.errors
     });
     var errors = this.props.errors;
-
     var errorsList = errors ? errors.map(function(error){
       return(
         <span className="help-block error" key={error}>
