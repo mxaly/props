b548ae00cfcd81454f4c466c4c5589061d88c10f
Kuba Niechciał <jakub.niechcial@netguru.co>
2015-07-24 09:33:56 +0200

    use label instead of value

--- app/assets/javascripts/components/prop-user-option.js.jsx
+++ app/assets/javascripts/components/prop-user-option.js.jsx
@@ -4,7 +4,6 @@ class Option extends React.Component {
 
   render() {
     const obj = this.props.option;
-    console.log(obj);
     return (
       <div
         className={this.props.className}
@@ -13,7 +12,7 @@ class Option extends React.Component {
         onMouseDown={this.props.mouseDown}
         onClick={this.props.mouseDown}>
         <img className="user-small-face" src={obj.avatarUrl}></img>
-        <span>{obj.value}</span>
+        <span>{obj.label}</span>
       </div>
     );
   }
